c7048499059c99916a81818a2c2f1e281c1d4722
Jacob Dunbar <jacobfdunbar@gmail.com>
2023-11-09 15:16:32 -0500

    Adding delayable states to StateEnabledCanvas.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
 ==
-## [1.0.2] - 2023-11-01
+## [1.0.4] - 2023-11-09
+### Added
+- Can now set delay states for StateEnabledCanvas
+
+## [1.0.3] - 2023-11-01
 ### Fixed
 - Making BoolEnabledCanvas enable/disable the canvas in OnEnable().
 
--- Runtime/UI/StateEnabledCanvas.cs
+++ Runtime/UI/StateEnabledCanvas.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using System.Collections.Generic;
 using GI.UnityToolkit.State;
+using GI.UnityToolkit.Utilities;
 using UnityEngine;
 
 #if ODIN_INSPECTOR
 using Sirenix.OdinInspector;
+#else
+using NaughtyAttributes;
 #endif
 
 namespace GI.UnityToolkit.Components.UI
@@ -11,20 +15,42 @@ namespace GI.UnityToolkit.Components.UI
     /// <summary>
     /// Enables and disables an attached Canvas based on the state.
     /// </summary>
-    [AddComponentMenu("Grimbar Interactive/UI/State Canvas")]
+    [AddComponentMenu("Grimbar Interactive/UI/State Enabled Canvas")]
     public class StateEnabledCanvas : CanvasComponent, IStateListener<State.State>
     {
         [SerializeField] private StateManager stateManager;
         
 #if ODIN_INSPECTOR
-        private bool StateManagerIsNull => stateManager == null;
         private List<State.State> StateOptions => stateManager != null ? stateManager.States : new List<State.State>();
 
-        [SerializeField, HideIf(nameof(StateManagerIsNull)), ValueDropdown(nameof(StateOptions)), Space(4)]
+        [SerializeField, ShowIf(nameof(StateManagerIsSet)), ValueDropdown(nameof(StateOptions)), Space(4)]
 #else
-        [SerializeField, Space(4)]
+        [SerializeField, ShowIf(nameof(StateManagerIsSet)), Space(4)]
 #endif
         protected List<State.State> activeStates;
+
+#if ODIN_INSPECTOR
+        [SerializeField, ShowIf(nameof(StateManagerIsSet)), Space(4)]
+#else
+        [SerializeField, HorizontalLine(), ShowIf(nameof(StateManagerIsSet)), Space(4)]
+#endif    
+        private bool delayDisable = false;
+        
+#if ODIN_INSPECTOR
+        [SerializeField, ShowIf("@this.StateManagerIsSet && this.delayDisable"), ValueDropdown(nameof(StateOptions)), Space(4)]
+#else
+        [SerializeField, ShowIf(EConditionOperator.And,nameof(StateManagerIsSet), nameof(delayDisable)), Space(4)]
+#endif
+        protected List<State.State> delayedStates;
+        
+#if ODIN_INSPECTOR
+        [SerializeField, ShowIf("@this.StateManagerIsSet && this.delayDisable"), Space(4)]
+#else
+        [SerializeField, ShowIf(EConditionOperator.And,nameof(StateManagerIsSet), nameof(delayDisable)), Space(4)]
+#endif
+        protected float delayDuration = 1f;
+
+        private Coroutine _delayRoutine;
 
         private void OnEnable()
         {
@@ -39,7 +65,40 @@ namespace GI.UnityToolkit.Components.UI
 
         public void OnStateChanged(State.State previousState, State.State newState)
         {
-            Canvas.enabled = activeStates.Contains(newState);
+            if (_delayRoutine != null)
+            {
+                StopCoroutine(_delayRoutine);
+                _delayRoutine = null;
+            }
+            
+            // Enable the Canvas if we've entered an active state.
+            if (activeStates.Contains(newState))
+            {
+                Canvas.enabled = true;
+                return;
+            }
+
+            // If we're in an inactive state but the Canvas is already disabled, there's nothing else to do.
+            if (!Canvas.enabled) return;
+
+            // If we're in an inactive state but there's no disable delay or we're not in a delayed inactive state,
+            // just disable the Canvas immediately.
+            if (delayDisable == false || delayDuration <= 0 || delayedStates.Contains(newState) == false)
+            {
+                Canvas.enabled = false;
+                return;
+            }
+
+            _delayRoutine = StartCoroutine(WaitBeforeDisable(delayDuration));
+
+            IEnumerator WaitBeforeDisable(float duration)
+            {
+                yield return Wait.Time(duration);
+                Canvas.enabled = false;
+                _delayRoutine = null;
+            }
         }
+        
+        private bool StateManagerIsSet => stateManager != null;
     }
 }
