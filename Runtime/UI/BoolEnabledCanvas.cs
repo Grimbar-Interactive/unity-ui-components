23a0cbb02ee40de7e245392d804f251a182d0438
Jacob Dunbar <jacobfdunbar@gmail.com>
2023-11-16 11:13:33 -0500

    Adding ability to delay BoolEnabledCanvas disabling.

--- Runtime/UI/BoolEnabledCanvas.cs
+++ Runtime/UI/BoolEnabledCanvas.cs
@@ -1,5 +1,13 @@
+using System.Collections;
+using GI.UnityToolkit.Utilities;
 using GI.UnityToolkit.Variables;
 using UnityEngine;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#else
+using NaughtyAttributes;
+#endif
 
 namespace GI.UnityToolkit.Components.UI
 {
@@ -9,6 +17,22 @@ namespace GI.UnityToolkit.Components.UI
     public class BoolEnabledCanvas : CanvasComponent
     {
         [SerializeField] private BoolVariable boolVariable;
+        
+#if ODIN_INSPECTOR
+        [SerializeField, ShowIf(nameof(BoolVariableIsSet)), Space(4)]
+#else
+        [SerializeField, HorizontalLine(), ShowIf(nameof(BoolVariableIsSet)), Space(4)]
+#endif    
+        private bool delayDisable = false;
+
+#if ODIN_INSPECTOR
+        [SerializeField, ShowIf("@this.BoolVariableIsSett && this.delayDisable"), Space(4)]
+#else
+        [SerializeField, ShowIf(EConditionOperator.And,nameof(BoolVariableIsSet), nameof(delayDisable)), Space(4)]
+#endif
+        protected float delayDuration = 1f;
+
+        private Coroutine _delayRoutine;
 
         private void OnEnable()
         {
@@ -23,7 +47,36 @@ namespace GI.UnityToolkit.Components.UI
 
         private void OnValueChanged()
         {
-            Canvas.enabled = boolVariable.Value;
+            if (_delayRoutine != null)
+            {
+                StopCoroutine(_delayRoutine);
+                _delayRoutine = null;
+            }
+
+            if (boolVariable.Value)
+            {
+                Canvas.enabled = true;
+                return;
+            }
+
+            if (!Canvas.enabled) return;
+
+            if (delayDisable == false || delayDuration <= 0)
+            {
+                Canvas.enabled = false;
+                return;
+            }
+            
+            _delayRoutine = StartCoroutine(WaitBeforeDisable(delayDuration));
+
+            IEnumerator WaitBeforeDisable(float duration)
+            {
+                yield return Wait.Time(duration);
+                Canvas.enabled = false;
+                _delayRoutine = null;
+            }
         }
+        
+        private bool BoolVariableIsSet => boolVariable != null;
     }
 }
